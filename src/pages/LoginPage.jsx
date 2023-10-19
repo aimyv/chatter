518333535ce0bc70feb68b43d21e41d503a98018
aimyv <av533@bath.ac.uk>
2023-10-19 20:23:59 +0100

    added login form

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { userAuth } from '../utils/AuthContext'
 import { useNavigate } from 'react-router-dom'
 
@@ -6,14 +6,51 @@ const LoginPage = () => {
     const { user } = userAuth()
     const navigate = useNavigate()
 
+    const [credentials, setCredentials] =  useState({
+        email: '',
+        password:  ''
+    })
+
     useEffect(() => {
         if (user){
             navigate('/')
         }
     }, [])
 
+    const  handleInputChange = (e)  => {
+        let name = e.target.name
+        let value =  e.target.value
+
+        setCredentials({...credentials, [name]: value})
+        console.log(credentials)
+    }
+
     return (
-        <div>LoginPage</div>
+        <div className='auth--container'>
+            <div className='form-wrapper'>
+                <form>
+                    <div className='field--wrapper'>
+                        <label>Email:</label>
+                        <input type='email' 
+                        required 
+                        name='email' 
+                        placeholder='Enter your email'
+                        value={credentials.email}
+                        onChange={handleInputChange} />
+                    </div>
+
+                    <div className='field--wrapper'>
+                        <label>Password:</label>
+                        <input type='password' 
+                        required 
+                        name='password' 
+                        placeholder='Enter your password'
+                        value={credentials.password}
+                        onChange={handleInputChange} />
+                    </div>
+                </form>
+            </div>
+        </div>
     )
 }
 
